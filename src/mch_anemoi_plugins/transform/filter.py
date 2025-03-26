1e8a60be152b9400b78f3d991ed55ff1d3ec7875
Francesco Zanetta <zanetta.francesco@gmail.com>
2025-03-26 13:31:51 +0100

    add example plugin

--- src/mch_anemoi_plugins/transform/filter.py
+++ src/mch_anemoi_plugins/transform/filter.py
@@ -64,3 +64,43 @@ class HorizontalDestagger(Filter):
 
     def backward_transform(self):
         raise NotImplementedError("HorizontalDestagger is not reversible.")
+
+
+class ExamplePlugin(Filter):
+    """A filter to do something on fields."""
+
+    # The version of the plugin API, used to ensure compatibility
+    # with the plugin manager.
+
+    api_version = "1.0.0"
+
+    # The schema of the plugin, used to validate the parameters.
+    # This is a Pydantic model.
+
+    schema = None
+
+    def __init__(self, factor: float = 2.0):
+        """Initialise the filter with the user's parameters"""
+
+        self.factor = factor
+
+    def forward(self, data: ekd.FieldList) -> ekd.FieldList:
+        """Multiply all field values by self.factor"""
+
+        result = []
+        for field in data:  # Loop over all fields in the input data
+            values = (
+                field.to_numpy() * self.factor
+            )  # Multiply the field values by self.factor
+
+            out = new_field_from_numpy(
+                values,
+                template=field,  # Use the input field as a template for the output field
+                param=field.metadata("param")
+                + "_modified",  # Add "_modified" to the parameter name
+            )
+
+            result.append(out)
+
+        # Return the modified fields
+        return new_fieldlist_from_list(result)
