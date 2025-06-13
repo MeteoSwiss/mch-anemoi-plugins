b939bd933cbb3179cadc09fc235e04b86c1a0697
omiralles <ophelia.miralles@meteoswiss.ch>
2025-06-13 12:00:55 +0200

    Add tests for sources

--- src/mch_anemoi_plugins/transform/sources.py
+++ src/mch_anemoi_plugins/transform/sources.py
@@ -1,5 +1,7 @@
 import datetime
 import json
+from itertools import chain
+
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -10,24 +12,15 @@ from anemoi.datasets.create.sources.xarray_support.fieldlist import XarrayFieldL
 from anemoi.datasets.create.sources.xarray_support.flavour import CoordinateGuesser
 from anemoi.datasets.create.sources.xarray_support.time import Time
 from anemoi.datasets.create.sources.xarray_support.variable import Variable
+from data_provider.default_provider import all_retrievers
 from data_provider.default_provider import default_provider
 from data_provider.utils import read_file
 from gridefix_process.helpers import reproject
 from pyproj import CRS
 
 
-def get_opera_target_grid():
-    # target grid: Regular lat lon grid
-    custom_target_grid = xr.Dataset(
-        {"x": np.arange(0.5, 16.5, 0.01), "y": np.arange(43, 50, 0.01)},
-        attrs={
-            "grid_mapping": {
-                "epsg_code": "4326",
-            },
-            "source": "Custom grid",
-        },
-    )
-    return custom_target_grid
+def get_all_data_provider_sources():
+    return list(set(chain.from_iterable(r.sources for r in all_retrievers())))
 
 
 def assign_lonlat(array, crs):
@@ -37,6 +30,7 @@ def assign_lonlat(array, crs):
 
 
 class MCHVariable(Variable):
+    """A variable class for MCH data, extending the XArrayVariable class with more metadata."""
 
     def __init__(
         self,
@@ -62,7 +56,9 @@ class MCHVariable(Variable):
         )
         self.proj_string = proj_string
         self.source = source
-        self._metadata = {x.replace("variable", "param"): k for x, k in self._metadata.items()}
+        self._metadata = {
+            x.replace("variable", "param"): k for x, k in self._metadata.items()
+        }
 
     def __getitem__(self, i):
         if i >= self.length:
@@ -87,7 +83,9 @@ class MCHField(XArrayField):
         station_or_cell = ["cell", "station"]
         data_coords = [c for c in self.selection.coords]
         data_dims = [c for c in self.selection.dims]
-        return np.intersect1d(std_grid_coords + station_or_cell, data_coords + data_dims)
+        return np.intersect1d(
+            std_grid_coords + station_or_cell, data_coords + data_dims
+        )
 
     @property
     def not_grid_dim(self):
@@ -100,7 +98,9 @@ class MCHField(XArrayField):
         else:
             valid_crs = CRS.from_user_input(self.crs)
         valid_crs = CRS.from_user_input("epsg:2056")
-        minimal = self.selection.isel({d: 0 for d in self.not_grid_dim}).rio.write_crs(valid_crs)
+        minimal = self.selection.isel({d: 0 for d in self.not_grid_dim}).rio.write_crs(
+            valid_crs
+        )
         if minimal.rio.crs.is_projected:
             minx = minimal.x.to_numpy()
             minx.sort()
@@ -111,7 +111,14 @@ class MCHField(XArrayField):
             res_m = np.array([minx, miny]) * minimal.rio.crs.units_factor[1]
         else:
             res_deg = np.array(minimal.rio.resolution())
-            res_m = res_deg * np.diff(reproject([0, 1], [0, 0], valid_crs, CRS.from_user_input("epsg:2056")))[0][0]
+            res_m = (
+                res_deg
+                * np.diff(
+                    reproject(
+                        [0, 1], [0, 0], valid_crs, CRS.from_user_input("epsg:2056")
+                    )
+                )[0][0]
+            )
         res_km = np.round(res_m / 1e3, 0)
         return f"{tuple(v.item() for v in res_km)[0]}km"
 
@@ -132,7 +139,6 @@ class MCHField(XArrayField):
 
 
 class MCHFieldList(XarrayFieldList):
-
     @classmethod
     def from_xarray(cls, ds, flavour=None, proj_string=None, source=""):
         variables = []
@@ -183,14 +189,20 @@ class MCHFieldList(XarrayFieldList):
         return cls(ds, variables)
 
 
-def get_fieldlist_from_data_provider(provider, source, dates, param, **retriever_kwargs):
+def get_fieldlist_from_data_provider(
+    provider, source, dates, param, **retriever_kwargs
+):
     expanded_kwargs = retriever_kwargs.copy()
     for k, v in retriever_kwargs.items():
         if isinstance(v, str) and v.startswith("$file:"):
             expanded_kwargs[k] = read_file(v.removeprefix("$file:"))
     data = provider.provide(source, param, dates, **expanded_kwargs)
     time_dim = (
-        "forecast_reference_time" if "forecast_reference_time" in data.dims else "time" if "time" in data.dims else None
+        "forecast_reference_time"
+        if "forecast_reference_time" in data.dims
+        else "time"
+        if "time" in data.dims
+        else None
     )
     data = data.drop_duplicates(...)
     crs = provider.get_crs(source)
@@ -205,9 +217,13 @@ def get_fieldlist_from_data_provider(provider, source, dates, param, **retriever
         crs = "epsg:2056"
     if not ("longitude" in data.coords and "latitude" in data.coords):
         data = assign_lonlat(data, crs)
-    if len(dates) == 1:  # minimal input selects first day at midnight even if time is missing...
+    if (
+        len(dates) == 1
+    ):  # minimal input selects first day at midnight even if time is missing...
         first_hour_day = [d for d in data[time_dim].dt.round("1d").to_numpy()]
-        data = data.reindex_like(xr.Dataset(coords={time_dim: first_hour_day}), method="nearest")
+        data = data.reindex_like(
+            xr.Dataset(coords={time_dim: first_hour_day}), method="nearest"
+        )
     potentially_misleading_coords = [
         "lead_time",
         "forecast_reference_time",
@@ -221,8 +237,13 @@ def get_fieldlist_from_data_provider(provider, source, dates, param, **retriever
         if n in data.coords and n not in data.dims:
             data = data.drop(n)
     data = data.rename({time_dim: "forecast_reference_time"})
-    if data.forecast_reference_time.to_series().duplicated().any() and source != "SATELLITE":
-        data = data.groupby("forecast_reference_time").mean()  # avoids duplicated dates...
+    if (
+        data.forecast_reference_time.to_series().duplicated().any()
+        and source != "SATELLITE"
+    ):
+        data = data.groupby(
+            "forecast_reference_time"
+        ).mean()  # avoids duplicated dates...
     if "lead_time" in data.dims:
         # forecast data
         data["lead_time"].attrs.update(standard_name="forecast_period")
@@ -239,8 +260,12 @@ def get_fieldlist_from_data_provider(provider, source, dates, param, **retriever
 
 
 class DataProviderSource(Source):
+    """Base source class for data provider sources in Anemoi."""
+
     def __init__(self, context, source, param, **retriever_kwargs):
-        super().__init__(context, source, param=param, retriever_kwargs=retriever_kwargs)
+        super().__init__(
+            context, source, param=param, retriever_kwargs=retriever_kwargs
+        )
         self.provider = default_provider()
         self.source = source
         self.param = param
@@ -259,51 +284,51 @@ class DataProviderSource(Source):
         return fieldlist
 
 
-class COSMO1E(DataProviderSource):
+def make_source_class(source_name: str):
     def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "COSMO-1E", param, **retriever_kwargs)
+        super(self.__class__, self).__init__(
+            context, source_name, param, **retriever_kwargs
+        )
+
+    class_name = source_name.replace("-", "_")
+    return type(class_name, (DataProviderSource,), {"__init__": __init__})
 
 
-class RADAR(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "RADAR", param, **retriever_kwargs)
+def get_all_source_classes(source_names: list[str]) -> dict[str, type]:
+    """
+    Create source classes for all specified source names.
+
+    Args:
+        source_names (list[str]): A list of source names to create classes for.
+
+    Returns:
+        dict[str, Type[DataProviderSource]]: A dictionary mapping source names to their
+            dynamically created classes.
+    """
+    return {name: make_source_class(name) for name in source_names}
 
 
-class ICON_CH1_EPS(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "ICON-CH1-EPS", param, **retriever_kwargs)
-
-
-class INCA(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "INCA", param, **retriever_kwargs)
-
-
-class SURFACE(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "SURFACE", param, **retriever_kwargs)
-
-
-class NASADEM(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "NASADEM", param, **retriever_kwargs)
-
-
-class SATELLITE(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "SATELLITE", param, **retriever_kwargs)
-
-
-class KENDA_CH1(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "KENDA-CH1", param, **retriever_kwargs)
-
-
-class GEOSATCLIM(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "GEOSATCLIM", param, **retriever_kwargs)
-
-
-class OPERA(DataProviderSource):
-    def __init__(self, context, param, **retriever_kwargs):
-        super().__init__(context, "OPERA", param, **retriever_kwargs)
+source_names = get_all_data_provider_sources()
+source_classes = get_all_source_classes(source_names)
+# This will print the source class definitions
+# Still have to execute this code to make sure the classes are created and available for the pyproject
+print(
+    "\n".join(
+        f'{name.replace("-", "_")} = make_source_class("{name}")'
+        for name in source_names
+    )
+)
+COSMO_E = make_source_class("COSMO-E")
+INCA = make_source_class("INCA")
+OPERA = make_source_class("OPERA")
+GEOSATCLIM = make_source_class("GEOSATCLIM")
+KENDA_CH1 = make_source_class("KENDA-CH1")
+RADAR = make_source_class("RADAR")
+DHM25 = make_source_class("DHM25")
+COSMO_1 = make_source_class("COSMO-1")
+CEDTM = make_source_class("CEDTM")
+CMSAF = make_source_class("CMSAF")
+SATELLITE = make_source_class("SATELLITE")
+NASADEM = make_source_class("NASADEM")
+ICON_CH1_EPS = make_source_class("ICON-CH1-EPS")
+SURFACE = make_source_class("SURFACE")
---
+++ tests/test_transform/test_sources.py
@@ -0,0 +1,46 @@
+import entrypoints
+import pytest
+from anemoi.datasets.create import Init
+from data_provider.default_provider import all_retrievers
+
+from mch_anemoi_plugins.transform.sources import DataProviderSource
+
+all_entrypoints = entrypoints.get_group_all("anemoi.datasets.sources")
+all_sources = [e.name for e in all_entrypoints]
+all_classes = [e.object_name.replace("_", "-") for e in all_entrypoints]
+example_variable = {
+    e: next(list(r.variables.keys())[0] for r in all_retrievers() if s in r.sources)
+    for (s, e) in zip(all_classes, all_sources)
+}
+
+
+@pytest.mark.parametrize("source_name", all_sources)
+def test_data_provider_sources(source_name, tmp_path):
+    """Test that all data-provider sources can be loaded."""
+    source = entrypoints.get_single("anemoi.datasets.sources", source_name)
+    assert source is not None, f"Source {source_name} could not be loaded"
+
+    # test if can be instantiated
+    try:
+        instance = source.load()
+        assert issubclass(instance, DataProviderSource), (
+            f"Instance of {source_name} could not be created"
+        )
+    except Exception as e:
+        pytest.fail(f"Failed to create instance of {source_name}: {e}")
+
+    # test if we can create a dataset with this source
+    dataset_test_config = {
+        "dates": {"start": "2019-01-01", "end": "2019-01-02", "frequency": "1h"},
+        "input": {source.name: {"param": [example_variable[source.name]]}},
+        "build": {"group_by": "daily", "variable_naming": "param", "allow_nans": True},
+    }
+    try:
+        Init(
+            path=str(tmp_path.with_suffix(".zarr")),
+            config=dataset_test_config,
+            overwrite=True,
+        )
+        # c.run() if problems with run the data_provider is the cause
+    except Exception as e:
+        pytest.fail(f"Failed to run dataset initialization for {source_name}: {e}")
