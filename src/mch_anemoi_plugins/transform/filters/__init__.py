2055a9aa647a75088bc6c887b943dcaaa9f60693
Francesco Zanetta <francesco.zanetta@meteoswiss.ch>
2025-10-01 13:43:57 +0200

    add omega-from-w filter

--- src/mch_anemoi_plugins/transform/filters/__init__.py
+++ src/mch_anemoi_plugins/transform/filters/__init__.py
@@ -3,6 +3,7 @@ from .destaggering import Destagger
 from .grid import AssignGrid
 from .vertical_interpolation import InterpK2P
 from .horizontal_interpolation import Interp2Grid, InterpNAFilter, Interp2Res, Project
+from .omega_from_w import OmegaFromW
 
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     "InterpNAFilter",
     "Interp2Res",
     "Project",
+    "OmegaFromW",
 ]
