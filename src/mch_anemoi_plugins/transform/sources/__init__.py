ed1332b43e46012ddc890b18764a8ec5bb5350fa
omiralles <ophelia.miralles@meteoswiss.ch>
2025-11-03 15:00:06 +0100

    PC

--- src/mch_anemoi_plugins/transform/sources/__init__.py
+++ src/mch_anemoi_plugins/transform/sources/__init__.py
@@ -1,8 +1,7 @@
 from .realch1_precip import ReaLCh1Precip
-from .weathermart_sources import (
-    NASADEM,
-    OPERA,
-    SATELLITE,
-    SURFACE,
-)
+from .weathermart_sources import NASADEM
+from .weathermart_sources import OPERA
+from .weathermart_sources import SATELLITE
+from .weathermart_sources import SURFACE
+
 __all__ = ["ReaLCh1Precip", "NASADEM", "OPERA", "SATELLITE", "SURFACE"]
